de7bf0609538fd5028f422b2fa36c00673a6a17d
sashazabor8 <106959082+sashazabor8@users.noreply.github.com>
2023-04-26 13:41:58 +0300

    add 10 home-work-js

--- src/js/createMarkup.js
+++
@@ -1,6 +0,0 @@
-import menu from '../JSON/menu.json';
-import markupCards from '../hbs/markupCard.hbs';
-
-const jsMenuRef = document.querySelector('.js-menu');
-jsMenuRef.insertAdjacentHTML('beforeend', markupCards(menu))
-
